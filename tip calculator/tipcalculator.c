375b1bbc2c59291a4b8a209247d212d977cdff7e
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-12-03 18:01:43 +0100

    re-builded and

--- tip calculator/tipcalculator.c
+++ tip calculator/tipcalculator.c
@@ -1,41 +1,49 @@
+// requiered include
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    int total_bill;
-    float tip_percentage;
-    float bill_after_tip;
+int main(){
+//variable
+int total_bill;
+float tip_percentage;
+float bill_after_tip;
 
-    while (1) {
-        printf("What is the bill amount: \n");
-        scanf("%d", &total_bill);
-        printf("Your total bill without tip: %d\n", total_bill);
+//grtting the input for total bill
+while (1)
+{
+    printf("how much is the bill\n");
+    scanf("%d",&total_bill);
+    printf("is you total %d\n",total_bill);
 
-        char confirm_total_bill[3];
-        printf("Is that correct (yes/no)\n");
-        scanf("%s", confirm_total_bill);
-        if (strcmp(confirm_total_bill, "yes") == 0) {
-            break;
-        }
+// input to confirm this input
+    char confirm_total_bill[3];
+    printf("is this corecct (yes/no)\n");
+    scanf("%s",confirm_total_bill);
+    if(strcmp(confirm_total_bill,"yes")==0){
+        break;
     }
-
-    // Get the tip percentage
-    while (1) {
-        printf("How much do you want to tip:\n");
-        scanf("%f", &tip_percentage);
-        printf("You want to tip %f\n", tip_percentage);
+}
+ 
+//get tip percentage
+    while (1)
+    {
+        printf("how much percent of your bill do you tip\n");
+        scanf("%f",&tip_percentage);
+        printf("you what to tip%f\n",tip_percentage);
 
         char confirm_tip_percentage[3];
-        printf("Is this correct (yes/no):\n");
+        printf("is that correct(yes/no)\n");
         scanf("%s",confirm_tip_percentage);
-        if (strcmp(confirm_tip_percentage, "yes") == 0) {
+        if(strcmp(confirm_tip_percentage,"yes") ==0){
             break;
         }
     }
 
-//this calculatsn the bill with tip
-       tip_percentage = total_bill+(total_bill*tip_percentage/100);
-       printf("you bill with tip is: %f\n",tip_percentage);
+// this calculat the total bill
+    bill_after_tip = total_bill+(total_bill*tip_percentage/100);
+    printf("you bill with tip is %f\n",bill_after_tip);
+    
+    
+
     return 0;
 }
-    
