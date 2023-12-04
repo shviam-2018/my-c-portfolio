c74a08859908679444aa4968e81ffceeb476c868
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-12-04 17:17:31 +0100

    one last practise befor making it intyo a real project

--- practise/practise.c
+++ practise/practise.c
@@ -1,42 +1,41 @@
-//requiered include
+//requied include
 #include <stdio.h>
+#include <string.h>
 
+//creating main
 int main(){
-//declearing variables
-    int Human_age;
-    int dog_year = Human_age* 15;
-    int cat_years =Human_age*15;
+    // variable
+    int human_age;
+    int dog_year = 15;
+    int cat_years = 15;
 
-//getting user input and chaking if it is correcct
+// asking for user age
     while (1)
     {
-     printf("Hey, how old are you: \n");
-     scanf("%d",&Human_age);
-    printf("the age you entered is:%d \n",Human_age);
+    printf("enter your age: ");
+    scanf("%d",&human_age);
+    printf("the age you enterd is %d\n",human_age);
 
-    char confirm_human_age[3];
-    printf("is this corecct: %d\n",Human_age);
+// this is to confirm the age enterd by the user
+    char confirm_human_age[4];
+    printf("is this correct: ");
     scanf("%s", confirm_human_age);
-    if(strcmp(confirm_human_age,"yes") ==0){
+    if(strcmp(confirm_human_age, "yes") ==0){
         break;
+       }
     }
-    }
-
-//code to ask do they what to know there age in cat or dog years
+    
+// asking user for do they what to know there age in dog or cat years
     while (1)
     {
-        char cat_or_dog[3];
-        printf("do you what to know your age in dog or cat years: \n");
-        scanf("%s",cat_or_dog);
-        if (strcmp(cat_or_dog, "cat") ==0){
-            printf("your age in cat years is %d\n",cat_years);
-        }   
-        if (strcmp(cat_or_dog,"dog") ==0){ 
+        char dog_or_cat_year[4];
+        printf("do you what to know your age is cat or dog years: ");
+        scanf("%s", dog_or_cat_year);
+        if (strsmp(dog_or_cat_year,"dog") ==0){
             printf("you age in dog years is %d\n",dog_year);
         }
-            
+        
     }
     
     
-    return 0;
 }
