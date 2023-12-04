8edb113f509b028c548d0c9cdd5e28a5190cb27d
NightDev4l <122033083+shviam-2018@users.noreply.github.com>
2023-12-04 10:05:00 +0000

    new file potention new project

--- practise/practise.c
+++ practise/practise.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    printf("erorr compiling");
+    int Human_age;
+    int dog_year = 15;
+    int cat_years =15;
+
+    while (1)
+    {
+     printf("Hey, how old are you: \n");
+     scanf("%d",&Human_age);
+    printf("the age you entered is: \n",Human_age);
+
+    char confirm_human_age[3];
+    printf("is this corecct: \n");
+    scanf("%s", confirm_human_age);
+    if(strcmp(confirm_human_age,"yes") ==0){
+        break;
+    }
+    }
+
+    while (1)
+    {
+        //TODO: add code to ask if them what to know there age in cat or dag year
+    }
+    
+    
     return 0;
 }
