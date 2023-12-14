75c5acc798665784c61d67a1e847e3885c0b3c13
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-12-14 19:01:25 +0100

    erased

--- practise/practise.c
+++ practise/practise.c
@@ -2,40 +2,6 @@
 #include <string.h>
 
 int main(){
-    int  bill_before_tip;
-    float tip_percentage;
-    float Bill_after_tip;
-
-//this is to get user input for total bill before tip
-    while (1)
-    {
-    printf("enter total bill: ");
-    scanf("%d",&bill_before_tip);
-    printf("the ammount you entered is: %d\n",bill_before_tip);
-
-    char confirm_before_bill[4];
-    printf("is this correct(yes/no): %s");
-    scanf("%s",confirm_before_bill);
-    if(strcmp(confirm_before_bill,"yes")==0){
-        break;
-    }
-
-//this is to get tippercentage from user
-    while (1)
-    {
-        printf("who much of the ammount do you what to tip: ");
-        scanf("%f",&tip_percentage);
-        printf("yoou whant to tip: %f\n",tip_percentage);
-
-        char confirm_tip_persentage[3];
-        printf("is this correct: ");
-        scanf("%s", confirm_tip_persentage);
-        if(strcmp(confirm_tip_persentage,"yes")==0){
-            break;
-        }
-    }
-    
-}
-    
+    //template ready for  code
     return 0;
 }
