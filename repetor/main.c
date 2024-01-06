bdbfa56b74d08e9c1a541f8435a76865d4680fa8
NightDev4l <122033083+shviam-2018@users.noreply.github.com>
2024-01-06 20:47:47 +0000

    fixed mistakes

--- repetor/main.c
+++ repetor/main.c
@@ -1,45 +1,48 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
+// Declare the displayresult function before using it
+void displayresult(const char* message, int times);
+
+int main() {
     char What_to_say[1000];
     int times;
 
-    while (1)
-    {
-        printf("what do you what me to say: ");
-        scanf("%s",What_to_say);
-        printf("you what me to say: ",What_to_say,"?");
+    while (1) {
+        printf("What do you want me to say: ");
+        scanf("%s", What_to_say);
+        printf("You want me to say: %s?\n", What_to_say);  // Fixing the printf statement
 
-        char confirm_WTS;
-        printf("is this correct(y/n): ");
+        char confirm_WTS[2];  // Change from char to char array
+        printf("Is this correct (y/n): ");
         scanf("%s", confirm_WTS);
-        if(strcmp(confirm_WTS, "y") ==0){
+        if (strcmp(confirm_WTS, "y") == 0) {
             break;
         }
     }
 
-    while (1)
-    {
-        printf("how many times do you what me to say: ",What_to_say);
-        printf("enter number of times: ");
-        scanf("%d", times);
-        printf("you what me to say: ",What_to_say," ",times,"times");
+    while (1) {
+        printf("How many times do you want me to say: ");
+        printf("Enter number of times: ");
+        scanf("%d", &times);  // Use the address of the variable for scanf
+        printf("You want me to say: %s %d times\n", What_to_say, times);  // Fixing the printf statement
 
-        char confirm_T;
-        printf("is this correct(y/n): ");
-        scanf("%s",confirm_T);
-        if(strcmp(confirm_T, "y") ==0) {
+        char confirm_T[2];  // Change from char to char array
+        printf("Is this correct (y/n): ");
+        scanf("%s", confirm_T);
+        if (strcmp(confirm_T, "y") == 0) {
             break;
-            
         }
     }
-    displayresult(What_to_say, times)
+
+    // Call the displayresult function with the correct arguments
+    displayresult(What_to_say, times);
+
     return 0;
 }
 
-void displayresult(const string&message, times) {
-    for(int i=0; i>times; ++i){
-        printf(i," ", What_to_say);
+void displayresult(const char* message, int times) {
+    for (int i = 0; i < times; ++i) {
+        printf("%d %s\n", i + 1, message);  // Fixing the printf statement
     }
 }
