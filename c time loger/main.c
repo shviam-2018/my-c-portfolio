fd7402d8b92ee195aadfe02d46f35f2998b962ec
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2024-01-08 18:37:03 +0100

    speed build completed and started on new project

---
+++ c time loger/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int hours;
+    int minutes;
+    int secoends;
+
+    printf("congratial on your speed build today.");
+    printf('hours: \n');
+    scanf("%d", hours);
+
+    return 0;
+}
