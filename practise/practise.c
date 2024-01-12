17135edcc9b011447f52eee1ef7882bb868f637f
NightDev4l <122033083+shviam-2018@users.noreply.github.com>
2024-01-12 22:20:51 +0000

    completed speed build with 9m 8s and logged

--- practise/practise.c
+++ practise/practise.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
-void displayresult(const char* message, int x);
+void displayresult(const char* message, int times);
 
 int main(){
-    char WTS[1000];
-    int Times;
+    char What_to_say[1000];
+    int time;
 
     while (1)
     {
-        printf("what do you what to say: ");
-        scanf("%s", WTS);
-        printf("you what me to say: %s\n", WTS);
+        printf("what do you what me to say: ");
+        scanf("%s", What_to_say);
+        printf("you want me to say: %s\n", What_to_say);
 
-        char confirm_WTS[100];
-        printf("is this correct(y/n): ");
-        scanf("%s", confirm_WTS);
-        if(strcmp(confirm_WTS, "y") ==0) {
+        char confirm_What_to_say[100];
+        printf("is this correct(y/n):");
+        scanf("%s", confirm_What_to_say);
+        if(strcmp(confirm_What_to_say, "y") ==0) {
             break;
-        } 
+        }
     }
 
     while (1)
     {
-        printf("how many times do you what to say: %s\n", WTS);
+        printf("how many times do you what me to say: %s\n", What_to_say);
         printf("enter number of times: ");
-        scanf("%d", &Times);
-        printf("you what to say: %s %d times\n", WTS, Times);
+        scanf("%d", &time);
+        printf("you what me to say: %s %d times \n", What_to_say, time);
 
-        char confirm_T[100];
+        char confirm_time[1000];
         printf("is this correct(y/n): ");
-        scanf("%s", confirm_T);
-        if(strcmp(confirm_T, "y") ==0) {
+        scanf("%s", confirm_time);
+        if (strcmp(confirm_time, "y") ==0) {
             break;
         }
     }
     
     
-displayresult(WTS, Times);
+displayresult(What_to_say, time);
     return 0;
 }
 
-void displayresult(const char* message, int x) {
-    for(int i = 0; i < x; ++i) {
-        printf("%d%s\n", i + 1, message);
+void displayresult(const char* message, int times) {
+    for(int i = 0; i < times; ++i) {
+        printf("%d %s \n", i + 1, message);
     }
 }
